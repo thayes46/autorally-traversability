a70e00e8bff3838aea5b2171869ceafe8c2f69d4
Todd Hayes <thayes46@gatech.edu>
2020-10-15 22:00:11 -0400

    printing image size test

--- image_tests.py
+++ image_tests.py
@@ -13,6 +13,7 @@ bag = rosbag.Bag(fpath + fname)
 # Print out topics if needed
 topics = bag.get_type_and_topic_info()[1].keys()
 for topic, msg, t in bag.read_messages(topics=['/left_camera/image_raw']):
-    print(msg)
+    print("Height: %s", msg.width)
+    print("Width: %s", msg.height)
 
 bag.close()
