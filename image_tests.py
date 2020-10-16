7565e3a37ad68cd287b6c275e2d398b7d7c19523
Todd Hayes <thayes46@gatech.edu>
2020-10-15 21:44:57 -0400

    print test for raw image

--- image_tests.py
+++ image_tests.py
@@ -12,5 +12,7 @@ bag = rosbag.Bag(fpath + fname)
 
 # Print out topics if needed
 topics = bag.get_type_and_topic_info()[1].keys()
+for topic, msg, t in bag.read_messages(topics=['/left_camera/image_raw']):
+    print(msg)
 
 bag.close()
