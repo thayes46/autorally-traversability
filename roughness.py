49f9439ce5ac6c9948df85170813eaff87d59651
thayes46 <todd.hayeshayes@gmail.com>
2020-11-23 12:17:58 -0500

    Fixed tuning since I was looking at data where the vehicle wasn't moving

--- roughness.py
+++ roughness.py
@@ -21,11 +21,12 @@ disturbance_data = np.array([])
 roughness_data = np.array([])
 time = np.array([])
 t0 = 0
-dt = 0.2  # timestep analysed, to be tied into back projection time stamp
+dt = 1.0  # timestep analysed, to be tied into back projection time stamp
 running_squared_data = np.array([])  # measurements used in calculation of metric
-num_data_points = 1000
+num_data_points_cap = 12000
+num_data_points = num_data_points_cap
 for topic, msg, t in bag.read_messages(topics=['/imu/imu']):
-    if num_data_points == 1000:
+    if num_data_points == num_data_points_cap:
         t0 = msg.header.stamp.to_time()
     num_data_points = num_data_points - 1
     if num_data_points < 0:
@@ -36,9 +37,9 @@ for topic, msg, t in bag.read_messages(topics=['/imu/imu']):
     running_squared_data = np.append(running_squared_data, vertical_acceleration * vertical_acceleration)
     disturbance_data = np.append(disturbance_data, vertical_acceleration)
     time = np.append(time, current_time)
-    if current_time > 0.2:
+    if current_time > dt:
         # calculate metric
-        roughness_metric = (np.average(running_squared_data, returned=True))[0] * 10.0
+        roughness_metric = (np.average(running_squared_data, returned=True))[0]
         # print("roughness_metric: " + str(roughness_metric))
         roughness_data = np.append(roughness_data, roughness_metric)
         # take off LRU
@@ -51,7 +52,7 @@ while len(disturbance_data) > len(roughness_data):
 fig, ax = plt.subplots()
 ax.set_xlabel('time')
 ax.set_ylabel('z-axis disturbance and metric')
-ax.set_title('IMU-based roughness metric')
+ax.set_title('IMU-based roughness metric with timestep of 1s')
 disturbance, = ax.plot(time, disturbance_data, label="Disturbance")
 metric, = ax.plot(time, roughness_data, label="Roughness Metric")
 # ax.plot(time, disturbance_data, time, roughness_data)
